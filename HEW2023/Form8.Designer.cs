afb4d3daa21ac234eb9070db6d121792b74091d1
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-03-02 04:12:29 +0900

    ZXing.Net をインストールし、QR生成テストコード作成。

---
+++ HEW2023/Form8.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace HEW2023
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(597, 145);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(191, 190);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(561, 323);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form8";
+            this.Text = "QRコード生成";
+            this.Load += new System.EventHandler(this.Form8_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ HEW2023/Form8.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HEW2023
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingGenerate(String writeText)
+        {
+            //作成
+            var qr_write = new ZXing.BarcodeWriter();
+            //バーコードの種類を選択
+            qr_write.Format = ZXing.BarcodeFormat.QR_CODE;//QRコード
+            //サイズを調整
+            qr_write.Options.Height = 216;
+            qr_write.Options.Width = 216;
+            //マージン
+            qr_write.Options.Margin = 1;
+            //文字コード
+            qr_write.Options.Hints.Add(ZXing.EncodeHintType.CHARACTER_SET, "Shift_JIS");
+            //エラー訂正レベル
+            qr_write.Options.Hints.Add(ZXing.EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.L);
+            //生成
+            pictureBox.Image = qr_write.Write(writeText);
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
