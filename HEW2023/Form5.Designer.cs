f700ed0158e21388e449d9083bd37e985a1f8fdd
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-03-01 18:26:32 +0900

    検索画面のデザイン変更。

--- HEW2023/Form5.Designer.cs
+++ HEW2023/Form5.Designer.cs
@@ -35,19 +35,10 @@ namespace HEW2023
             this.author_textBox = new System.Windows.Forms.TextBox();
             this.title_textBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.category_comboBox = new System.Windows.Forms.ComboBox();
             this.recommendation_comboBox = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.title_radioButton = new System.Windows.Forms.RadioButton();
             this.author_radioButton = new System.Windows.Forms.RadioButton();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
             this.panel1.SuspendLayout();
@@ -96,45 +87,13 @@ namespace HEW2023
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(450, 451);
+            this.button1.Location = new System.Drawing.Point(448, 418);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 28;
             this.button1.Text = "検索";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 421);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 12);
-            this.label5.TabIndex = 32;
-            this.label5.Text = "購入日（開始）";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(109, 418);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 19);
-            this.textBox5.TabIndex = 31;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(209, 421);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 12);
-            this.label6.TabIndex = 34;
-            this.label6.Text = "　～　購入日（終了）";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(320, 418);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 19);
-            this.textBox6.TabIndex = 33;
             // 
             // category_comboBox
             // 
@@ -153,38 +112,6 @@ namespace HEW2023
             this.recommendation_comboBox.Size = new System.Drawing.Size(160, 20);
             this.recommendation_comboBox.TabIndex = 36;
             this.recommendation_comboBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.recommendation_comboBox_KeyPress);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(209, 456);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(105, 12);
-            this.label7.TabIndex = 40;
-            this.label7.Text = "　～　登録日（終了）";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(320, 453);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 19);
-            this.textBox3.TabIndex = 39;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(26, 456);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(77, 12);
-            this.label8.TabIndex = 38;
-            this.label8.Text = "登録日（開始）";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(109, 453);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 19);
-            this.textBox4.TabIndex = 37;
             // 
             // title_radioButton
             // 
@@ -210,16 +137,6 @@ namespace HEW2023
             this.author_radioButton.UseVisualStyleBackColor = true;
             this.author_radioButton.CheckedChanged += new System.EventHandler(this.author_radioButton_CheckedChanged);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(450, 420);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(75, 16);
-            this.checkBox1.TabIndex = 43;
-            this.checkBox1.Text = "ブックマーク";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // panel1
             // 
             this.panel1.Controls.Add(this.title_radioButton);
@@ -233,19 +150,10 @@ namespace HEW2023
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 493);
+            this.ClientSize = new System.Drawing.Size(573, 455);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.recommendation_comboBox);
             this.Controls.Add(this.category_comboBox);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -271,19 +179,10 @@ namespace HEW2023
         private System.Windows.Forms.TextBox author_textBox;
         private System.Windows.Forms.TextBox title_textBox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.ComboBox category_comboBox;
         private System.Windows.Forms.ComboBox recommendation_comboBox;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.RadioButton title_radioButton;
         private System.Windows.Forms.RadioButton author_radioButton;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Panel panel1;
     }
 }
