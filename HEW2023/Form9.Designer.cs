8f8c3c1e5fd5a74af8b5cd85d69656d36a3facc3
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-03-03 07:38:20 +0900

    webカメラを認識。

--- HEW2023/Form9.Designer.cs
+++ HEW2023/Form9.Designer.cs
@@ -29,19 +29,48 @@ namespace HEW2023
         /// </summary>
         private void InitializeComponent()
         {
+            this.inputCamera_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // inputCamera_button
+            // 
+            this.inputCamera_button.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.inputCamera_button.Location = new System.Drawing.Point(819, 478);
+            this.inputCamera_button.Name = "inputCamera_button";
+            this.inputCamera_button.Size = new System.Drawing.Size(196, 58);
+            this.inputCamera_button.TabIndex = 0;
+            this.inputCamera_button.Text = "Webカメラ起動";
+            this.inputCamera_button.UseVisualStyleBackColor = true;
+            this.inputCamera_button.Click += new System.EventHandler(this.inputCamera_button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 513);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form9
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1027, 614);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.inputCamera_button);
             this.Name = "Form9";
             this.Text = "QR読み込み";
+            this.Load += new System.EventHandler(this.Form9_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button inputCamera_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- HEW2023/Form9.cs
+++ HEW2023/Form9.cs
@@ -8,13 +8,79 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
 namespace HEW2023
 {
     public partial class Form9 : Form
     {
+
+        Dummy dummy = new Dummy();
+
+        private Mat frame;
+        private VideoCapture camera;
         public Form9()
         {
             InitializeComponent();
+        }
+
+        private void Form9_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            //pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void inputCamera_button_Click(object sender, EventArgs e)
+        {
+            //VideoCapture作成
+            using (camera = new VideoCapture())
+            {
+                //カメラの起動　
+                camera.Open(0);
+                //camera.Open(1);
+
+                if (!camera.IsOpened())
+                {
+                    //throw new Exception("capture initialization failed");
+                    dummy.MessageBox_("デバイス接続エラー","カメラを認識しませんでした。");
+                    this.Close();                
+                }
+
+                //画像取得用のMatを作成
+                frame = new Mat();
+
+                while (true)
+                {
+                    try
+                    {
+                        camera.Read(frame);
+                        if (frame.Empty())
+                        {
+                            break;
+                        }
+
+                        if (frame.Size().Width > 0)
+                        {
+                            //PictureBoxに表示　MatをBitMapに変換
+                            pictureBox1.Image = BitmapConverter.ToBitmap(frame);
+                        }
+
+                        int key = Cv2.WaitKey();
+
+                        if (this.IsDisposed)
+                        {
+                            break;
+                        }
+                    }
+
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                }
+
+            }
         }
     }
 }
